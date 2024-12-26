825ad84fa9249a432488340e770f6237acb1baea
Anjan Roy <hello@itzmeanjan.in>
2024-12-26 15:04:54 +0400

    Update tests to use new API of binary fuse filter for KV Map
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- tests/prop_test_bff_for_kv_map.cpp
+++ tests/prop_test_bff_for_kv_map.cpp
@@ -12,11 +12,9 @@ TEST(BinaryFuseFilterForKVMap, CreateFilterAndRecoverValuesWhenQueriedUsingKeys)
 
   std::vector<bff_kv_map_utils::bff_key_t> keys(size);
   std::vector<uint32_t> values(size, 0);
-
   generate_random_keys_and_values(keys, values, plaintext_modulo);
 
-  bff_kv_map::bff_for_kv_map_t filter(size);
-  EXPECT_TRUE(filter.construct(keys, values, plaintext_modulo, label));
+  bff_kv_map::bff_for_kv_map_t filter(keys, values, plaintext_modulo, label);
 
   for (size_t i = 0; i < size; i++) {
     const uint32_t recovered = filter.recover(keys[i]);
@@ -33,11 +31,9 @@ TEST(BinaryFuseFilterForKVMap, SerializeAndDeserializeFilter)
 
   std::vector<bff_kv_map_utils::bff_key_t> keys(size);
   std::vector<uint32_t> values(size, 0);
-
   generate_random_keys_and_values(keys, values, plaintext_modulo);
 
-  bff_kv_map::bff_for_kv_map_t filter(size);
-  EXPECT_TRUE(filter.construct(keys, values, plaintext_modulo, label));
+  bff_kv_map::bff_for_kv_map_t filter(keys, values, plaintext_modulo, label);
 
   std::vector<uint8_t> filter_as_bytes(filter.serialized_num_bytes());
   EXPECT_TRUE(filter.serialize(filter_as_bytes));
@@ -63,11 +59,9 @@ TEST(BinaryFuseFilterForKVMap, CheckBitsPerEntry)
 
   std::vector<bff_kv_map_utils::bff_key_t> keys(size);
   std::vector<uint32_t> values(size, 0);
-
   generate_random_keys_and_values(keys, values, plaintext_modulo);
 
-  bff_kv_map::bff_for_kv_map_t filter(size);
-  EXPECT_TRUE(filter.construct(keys, values, plaintext_modulo, label));
+  bff_kv_map::bff_for_kv_map_t filter(keys, values, plaintext_modulo, label);
 
   const size_t bpe = filter.bits_per_entry();
   EXPECT_LT(bpe, std::log2(plaintext_modulo) + 2);
