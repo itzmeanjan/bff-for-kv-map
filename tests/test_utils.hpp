82adb2595d4ee337b860c5aea0e8823d390f373d
Anjan Roy <hello@itzmeanjan.in>
2025-01-04 11:44:13 +0400

    Update function implementation so that number of keys and values can be different
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- tests/test_utils.hpp
+++ tests/test_utils.hpp
@@ -29,12 +29,14 @@ generate_random_keys_and_values(std::span<bff_kv_map_utils::bff_key_t> keys, std
   std::uniform_int_distribution<uint32_t> dist_u32(0, plaintext_modulo - 1);
   std::uniform_int_distribution<uint64_t> dist_u64;
 
-  for (size_t i = 0; i < keys.size(); i++) {
-    keys[i].words[0] = dist_u64(gen);
-    keys[i].words[1] = dist_u64(gen);
-    keys[i].words[2] = dist_u64(gen);
-    keys[i].words[3] = dist_u64(gen);
+  for (auto& key : keys) {
+    key.words[0] = dist_u64(gen);
+    key.words[1] = dist_u64(gen);
+    key.words[2] = dist_u64(gen);
+    key.words[3] = dist_u64(gen);
+  }
 
-    values[i] = dist_u32(gen);
+  for (auto& value : values) {
+    value = dist_u32(gen);
   }
 }
